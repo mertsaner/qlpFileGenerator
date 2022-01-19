5a0bcc553030ac3987165a22d1bd2626cf57e567
mertsaner <mertsaner@windowslive.com>
2022-01-19 09:30:45 +0300

    Update from Mac

---
+++ cmake-build-debug/CMakeFiles/qlpFile.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qlpFile.dir/main.cpp.o"
+  "CMakeFiles/qlpFile.dir/main.cpp.o.d"
+  "qlpFile"
+  "qlpFile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qlpFile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/qlpFile.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for qlpFile.
+# This may be replaced when dependencies are built.
